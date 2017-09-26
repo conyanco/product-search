1006cb2e09c121e97383d589589b1c0fde614cd6
conyanco <n.namatame@gmail.com>
2017-09-27 08:26:28 +0900

    app

--- app.js
+++ app.js
@@ -80,9 +80,9 @@ http.get(url, (res) => {
 
 	// ビューを表示
 	return response.render("./search/result-list.ejs", data);
-	}).catch((reason) => {
+	})/*.catch((reason) => {
 	// エラー処理
-	console.log(reason);
+	console.log(reason);*/
 	});
 	return router;
 	})());
