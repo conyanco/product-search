3ab4cb9938373b902ea96909bf510b50c3b1bea0
conyanco <n.namatame@gmail.com>
2017-10-02 08:34:52 +0900

    app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require('request');
 //const MongoClient = require("mongodb").MongoClient;
 const http = require('http');
 
@@ -25,7 +26,7 @@ app.use("/shop", (function () {
 	let router = express.Router();
          // GET: /shop/search
 	 router.get("/search", function (request, response) {
-//	 let URL = "mongodb://localhost:27017/test";
+/*	 let URL = "mongodb://localhost:27017/test";
 	 let MAX_ITEMS_PER_PAGE = 1;
 	 let query = request.query.q;		           
 	 let page =request.query.pg ? parseInt(request.query.pg) : 1;			// 検索クエリがない場合、初期表示
@@ -34,48 +35,23 @@ app.use("/shop", (function () {
 	}
 	// 検索クエリがある場合、データベースを検索して結果リストを表示
 /*	
-MongoClient.connect(URL).then((db) => {
-	return Promise.all([
-	// 検索総ヒット数
-	db.collection("shops").find({
-	name: new RegExp(`.*${query}.*`)
-	}).count(),
-	// 現在ページに表示する内容
-	db.collection("shops").find({
-	name: new RegExp(`.*${query}.*`)
-	}).skip((page - 1) * MAX_ITEMS_PER_PAGE).limit(MAX_ITEMS_PER_PAGE).toArray()
-	]);
-	}).then((results) => {
-	// ビューへ渡すデータを整形
-	let data = {
-	count: results[0],
-	list: results[1],
-	pagination: {
-	max: Math.ceil(results[0] / MAX_ITEMS_PER_PAGE),
-	current: page,
-	isFirst: page === 1,
-	isLast: page === Math.ceil(results[0] / MAX_ITEMS_PER_PAGE)
-	},
-	query: query
-	};
-*/
 
 
-http.get(url, (res) => {
-  let body = '';
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    body += chunk;
-  });
-  res.on('end', (res) => {
-    res = JSON.parse(body);
-    JSON.parse(JSON.stringify(res), function(key, value){
-      console.log(key, value);
+//var url = 'https://api.github.com/users/rsp';
 
-    });
-  }).on('error', (e) => {
-    console.log(e.message);
-  });
+request.get({
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      // data is already parsed as JSON:
+      console.log(data.html_url);
+    }
 });
 
 	// ビューを表示
