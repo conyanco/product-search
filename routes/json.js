610d473ba48a6a6aba58fe1cb237cd6339249aa5
conyanco <n.namatame@gmail.com>
2017-10-03 00:31:24 +0900

    json

--- routes/json.js
+++ routes/json.js
@@ -1,7 +1,10 @@
 'use strict';
-var request = require('request');
+const request = require('request');
+const base_url = 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?&type=all&image_size=300&affiliate_type=vc&affiliate_id=http%3a%2f%2fck%2ejp%2eap%2evaluecommerce%2ecom%2fservlet%2freferral%3fsid%3d3103511%26pid%3d882391693%26vc_url%3d&appid=dj0zaiZpPUZCV2h2YUZxdnZ5SCZzPWNvbnN1bWVyc2VjcmV0Jng9NmQ-';
+let category = '13457';
+let keyword = 'fashion';
+let url = base_url + keyword + '&category_id=' + category;
 
-var url = 'https://api.github.com/users/rsp';
 
 request.get({
     url: url,
